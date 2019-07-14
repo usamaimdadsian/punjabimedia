7f284e196868181d02bcbd365c44ce9f77813633
Usama Imdad <usamaimdadsian@gmail.com>
2019-07-14 05:53:48 +0500

    Successfully Change the Blog link in Social Icons

--- resources/views/includes/social_icons.blade.php
+++ resources/views/includes/social_icons.blade.php
@@ -3,7 +3,7 @@
         <ul>
             <li class="w3_twitter"><a href="https://twitter.com/StageDramasPk" target="_blank">Twitter <i class="fa fa-twitter"></i></a></li>
             <li class="w3_facebook"><a href="https://web.facebook.com/stageDramasPakistan" target="_blank">Facebook <i class="fa fa-facebook"></i></a></li>
-            <li class="w3_dribbble"><a href="https://blogpakistanistagedramas.wordpress.com" target="_blank">Blog<i class="fa fa-wordpress"></i></a></li>
+            <li class="w3_dribbble"><a href="http://pakistanistagedramas.cf" target="_blank">Blog<i class="fa fa-wordpress"></i></a></li>
         </ul>
     </nav>
 </div>
