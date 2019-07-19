17ebb047b6691dc4ac1b8ae11f6c6586375b065d
Usama Imdad <usamaimdadsian@gmail.com>
2019-07-19 18:23:47 +0500

    Fixed the Register redirect Error

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -97,4 +97,13 @@ class RegisterController extends Controller
         return $this->registered($request, $user)
             ?: redirect($this->redirectPath());
     }
+
+    public function redirectPath()
+    {
+        if (method_exists($this, 'redirectTo')) {
+            return $this->redirectTo();
+        }
+
+        return property_exists($this, 'redirectTo') ? $this->redirectTo : '/';
+    }
 }
